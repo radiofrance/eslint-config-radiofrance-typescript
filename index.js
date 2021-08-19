0180443f17b62c6acfa41268cf33852ecdf975fb
Mehdy Dara <mehdy.dara.fr@gmail.com>
2021-08-19 11:59:11 +0200

    Fix extend of xo (esnext removed from repo)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   plugins: ['promise'],
-  extends: ['xo/esnext', 'xo-typescript/space', 'plugin:promise/recommended'],
+  extends: ['xo', 'xo-typescript/space', 'plugin:promise/recommended'],
   rules: {
     'capitalized-comments': 'off',
     'no-console': 'error',
